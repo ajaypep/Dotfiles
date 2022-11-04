f21b98d6af4b549d47301647c41b26dc34a628ea
Ajay K <76083469+ajaypep@users.noreply.github.com>
2022-11-04 18:13:30 +0530

    Enable tab to select the completion in neovim nvim-cmp

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,10 @@
 vim.o.number = true
-vim.o.relativenumber=true
-mouse=nvi
-vim.o.tabstop=4
-vim.o.shiftwidth=4
+vim.o.relativenumber = true
+mouse = nvi
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
 vim.o.expandtab = true
-vim.o.laststatus=2
+vim.o.laststatus = 2
 vim.o.termguicolors = true
 vim.g.mapleader = " "
 
--- nvim/lua/packer_plugins.lua
+++ nvim/lua/packer_plugins.lua
@@ -1,18 +1,18 @@
 require("packer").startup(function()
-    use "wbthomason/packer.nvim"
-    use 'dracula/vim'
-    use 'dense-analysis/ale'
-    use 'sbdchd/neoformat'
-    use 'nvim-lualine/lualine.nvim'
-    use 'kyazdani42/nvim-web-devicons'
-    use 'romgrk/barbar.nvim'
-    use 'nvim-tree/nvim-tree.lua'
-    use 'neovim/nvim-lspconfig'
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/cmp-cmdline'
-    use 'hrsh7th/nvim-cmp'
-    use 'nvim-lua/plenary.nvim'
-    use 'jose-elias-alvarez/null-ls.nvim'
+	use("wbthomason/packer.nvim")
+	use("dracula/vim")
+	use("dense-analysis/ale")
+	use("sbdchd/neoformat")
+	use("nvim-lualine/lualine.nvim")
+	use("kyazdani42/nvim-web-devicons")
+	use("romgrk/barbar.nvim")
+	use("nvim-tree/nvim-tree.lua")
+	use("neovim/nvim-lspconfig")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-cmdline")
+	use("hrsh7th/nvim-cmp")
+	use("nvim-lua/plenary.nvim")
+	use("jose-elias-alvarez/null-ls.nvim")
 end)
