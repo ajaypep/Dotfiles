bb1b2988c3ea523b7635d6b3dcf8dd901c55bc0f
Ajay K <76083469+ajaypep@users.noreply.github.com>
2022-11-04 17:22:56 +0530

    Add completion in neovim for lua and c/c++

--- nvim/lua/packer_plugins.lua
+++ nvim/lua/packer_plugins.lua
@@ -7,5 +7,10 @@ require("packer").startup(function()
     use 'kyazdani42/nvim-web-devicons'
     use 'romgrk/barbar.nvim'
     use 'nvim-tree/nvim-tree.lua'
-
+    use 'neovim/nvim-lspconfig'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-cmdline'
+    use 'hrsh7th/nvim-cmp'
 end)
