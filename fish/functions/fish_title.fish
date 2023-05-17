d6e44a7ba5ddba69a392251b6f1f4813d141b21e
Ajay K <76083469+ajaypep@users.noreply.github.com>
2023-05-17 21:00:02 +0530

    Set default title of fish, remove MEM keyword from neofetch.conf

--- fish/functions/fish_title.fish
+++ fish/functions/fish_title.fish
@@ -2,6 +2,6 @@ function fish_title
     #set -q argv[1]; or set argv fish
     # Looks like ~/d/fish: git log
     # or /e/apt: fish
-    #echo (fish_prompt_pwd_dir_length=1 prompt_pwd): $argv;
-    echo Terminal
+    echo (fish_prompt_pwd_dir_length=1 prompt_pwd): $argv;
+    #echo Terminal
 end
