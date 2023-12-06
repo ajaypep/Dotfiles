4cfabf18a405943361b4e1d8747702b2fc7551b9
Ajay K <76083469+ajaypep@users.noreply.github.com>
2023-12-06 13:30:10 +0530

    Update LuaSnip and ibl

--- nvim/lua/lazy_plugins.lua
+++ nvim/lua/lazy_plugins.lua
@@ -39,7 +39,13 @@ local plugins = {
 	"hrsh7th/nvim-cmp",
 	"onsails/lspkind.nvim",
 	{ "lukas-reineke/indent-blankline.nvim" },
-	{ "L3MON4D3/LuaSnip" },
+	{
+		"L3MON4D3/LuaSnip",
+		-- follow latest release.
+		version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+		-- install jsregexp (optional!).
+		build = "make install_jsregexp",
+	},
 	{ "saadparwaiz1/cmp_luasnip" },
 	{ "numToStr/Comment.nvim" },
 	"lewis6991/gitsigns.nvim",
--- nvim/plugin/indentation_blankline-nvim.lua
+++ nvim/plugin/indentation_blankline-nvim.lua
@@ -1,8 +1,4 @@
 vim.opt.list = true
 
 require("ibl").setup {
-    show_current_context = true,
-    --show_current_context_start = true,
-    show_end_of_line = true,
-    space_char_blankline = " ",
 }
