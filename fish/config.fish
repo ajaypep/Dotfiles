9c26c4f8548aeb15378046e5b17de06bac50dc37
Ajay K <76083469+ajaypep@users.noreply.github.com>
2024-04-12 00:32:38 +0530

    config(starship): enable transience

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,11 @@
 set fish_greeting
+
+function starship_transient_prompt_func
+  starship module character
+end
+
 starship init fish | source
+enable_transience
+
 export GPG_TTY=(tty)
 export TERM="xterm-256color"
