ef995333a393902badb104354c6313cd456709eb
Ajay K <76083469+ajaypep@users.noreply.github.com>
2024-04-13 17:22:44 +0530

    config(neovim): enable spell check

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,6 +4,7 @@ vim.g.have_nerd_font = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.mouse = "a"
+vim.opt.spell = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.showmode = false
