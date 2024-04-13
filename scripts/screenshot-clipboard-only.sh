6a0a15168d7cb5242a5ad1d5fed9f03203e56705
Ajay K <76083469+ajaypep@users.noreply.github.com>
2024-04-13 21:13:18 +0530

    fix(neovim): fix neovim starting blank due to xclip

--- scripts/screenshot-clipboard-only.sh
+++ scripts/screenshot-clipboard-only.sh
@@ -1,2 +1,5 @@
-maim -u -s -m 1 -l -c 0.46,0.46,0.46,0.6 | xclip -selection clipboard -t image/png;
+img_path="/tmp/temp_$(date +%s).png";
+maim -u -s -m 1 -l -c 0.46,0.46,0.46,0.6 ${img_path};
+cb copy ${img_path};
+rm ${img_path};
 
--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,9 +1,10 @@
-tmpfilename="/tmp/temp_$(date +%s).png";
-maim -u -s -m 1 -l -c 0.46,0.46,0.46,0.6 | tee ${tmpfilename} | xclip -selection clipboard -t image/png;
+img_path="/tmp/temp_$(date +%s).png";
+maim -u -s -m 1 -l -c 0.46,0.46,0.46,0.6 ${img_path};
+cb copy ${img_path};
 
-if [ -s ${tmpfilename} ]; then
-	eog ${tmpfilename};
+if [ -s ${img_path} ]; then
+	gpicview ${img_path};
 fi
 
-rm ${tmpfilename};
+rm ${img_path};
 
